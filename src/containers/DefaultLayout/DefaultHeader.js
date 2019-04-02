977bec43793c10a8ae118448de33ca4dbcf689cb
creditfirst612 <lovekite612@outlook.com>
2019-04-02 16:11:50 +0800

    push

---
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+
+import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
+import logo from '../../assets/img/brand/logo.png'
+
+
+const propTypes = {
+  children: PropTypes.node,
+};
+
+const defaultProps = {};
+
+class DefaultHeader extends Component {
+  render() {
+
+    // eslint-disable-next-line
+    const { children, ...attributes } = this.props;
+
+    return (
+      <React.Fragment>
+        <AppSidebarToggler className="d-lg-none" display="md" mobile />
+        <AppNavbarBrand
+          full={{ src: logo, width: 89, height: 25, alt: 'Logo' }}
+          minimized={{ src: logo, width: 30, height: 30, alt: 'Logo' }}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+DefaultHeader.propTypes = propTypes;
+DefaultHeader.defaultProps = defaultProps;
+
+export default DefaultHeader;
